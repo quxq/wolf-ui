436af4731fe19567881e7bb5158f1c317310b574
nick.qu <1055589275@qq.com>
2017-11-24 00:43:34 +0800

    新增：异步树形表格的增加、删除功能

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,10 @@
-const AppConfig={
-    ApiBaseUrl:'http://192.168.1.240:8517',//http://47.100.17.237:8517/breipapi
-    name: '聚数云盘',
-    prefix: 'antdAdmin',
-    footerText: 'Ant Design Admin  © 2017 屈向前',
-    logo: '/ico/logo.png',
+
+const AppConfig = {
+  ApiBaseUrl: 'http://47.100.17.237/breipapi', // http://47.100.17.237:8517/breipapi http://192.168.1.240:8517
+  name: '聚数云盘',
+  prefix: 'antdAdmin',
+  footerText: 'Ant Design Admin  © 2017 屈向前',
+  logo: '/ico/logo.png',
 }
 
 export default AppConfig
