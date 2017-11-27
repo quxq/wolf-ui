073d144c2d426c3003703e3a9727507ba7432e1e
nick.qu <1055589275@qq.com>
2017-11-27 22:13:40 +0800

    实现重命名

--- src/routes/netdriver/allfile/AllfileDataGrid.js
+++ src/routes/netdriver/allfile/AllfileDataGrid.js
@@ -3,12 +3,51 @@
  */
 
 import { AntDataGrid, Component } from 'wolf'
-import { Menu, Dropdown,Icon} from 'antd'
+import { Menu, Dropdown, Icon, Input, Button } from 'antd'
+
+
+class EditCell extends Component {
+  state = {
+    v: '',
+  }
+  handleOk =() => {
+    this.props.value.name = this.state.v
+    this.props.value.editable = false
+    if (this.props.updateRecord) {
+      this.props.updateRecord(0,this.props.value)
+    }
+  }
+  handleCancel =() => {
+    this.props.value.editable = false
+    if (this.props.updateRecord) {
+      this.props.updateRecord(1)
+    }
+  }
+  onChange =(e) => {
+    this.setState({ v: e.target.value })
+  }
+  render () {
+    return (<div>
+      {this.props.editable
+        ? <span>
+          <span style={{ margin: '-5px 5px', display: 'inline-block' }}><Input defaultValue={this.props.value.name} onChange={this.onChange} /></span>
+          <Button.Group style={{ margin: '-5px 5px', display: 'inline-block' }}>
+            <Button onClick={this.handleOk}><Icon type="check" />OK</Button>
+            <Button onClick={this.handleCancel}><Icon type="close" />Cancel</Button>
+          </Button.Group>
+        </span>
+        : this.props.value.name
+      }
+    </div>)
+  }
+}
+
 
 export default class AllfileDataGrid extends Component {
   state = {
     selectedRowKeys: [], // Check here to configure the default column
-    overkey:0,
+    overkey: 0,
+    editable: false,
   }
   /**
    *
@@ -23,7 +62,6 @@ export default class AllfileDataGrid extends Component {
       key: 'name',
       width: '600px',
       render: (text, record) => {
-
         const menu = (
           <Menu>
             <Menu.Item>
@@ -36,27 +74,29 @@ export default class AllfileDataGrid extends Component {
               <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">复制</a>
             </Menu.Item>
             <Menu.Item>
-              <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">重命名</a>
+              <a onClick={()=>{this.editCell(record)}}>重命名</a>
             </Menu.Item>
           </Menu>
         )
 
-        let tools;
-        if(this.state.overkey===record.files_id){
-          tools=(<span><Icon type="share-alt" style={{ fontSize: 16, color: '#08c' }} /> <Dropdown overlayStyle={{marginLeft:'40px'}} overlay={menu}><Icon type="ellipsis" style={{marginLeft:'16px', fontSize: 16, color: '#08c' }}/></Dropdown></span>)
+        let tools
+        if (this.state.overkey === record.files_id) {
+          tools = (<span><Icon type="share-alt" style={{ fontSize: 16, color: '#08c' }} /> <Dropdown overlayStyle={{ marginLeft: '40px' }} overlay={menu}><Icon type="ellipsis" style={{ marginLeft: '16px', fontSize: 16, color: '#08c' }} /></Dropdown></span>)
         }
-        let filetype;
-        if(record.f_t===3){
-          filetype=(<Icon type="file" style={{ fontSize: 24, color: '#c8be91' ,marginRight:16}} />)
+        let filetype
+        if (record.f_t === 3) {
+          filetype = (<Icon type="file" style={{ fontSize: 24, color: '#c8be91', marginRight: 16 }} />)
         }
-        if(record.f_t===2){
-          filetype=(<Icon type="folder" style={{ fontSize: 24, color: '#cca020',marginRight:16 }} />)
+        if (record.f_t === 2) {
+          filetype = (<Icon type="folder" style={{ fontSize: 24, color: '#cca020', marginRight: 16 }} />)
         }
-        if(record.f_t===1){
-          filetype=(<Icon type="folder-add" style={{ fontSize: 24, color: '#86cc32',marginRight:16 }} />)
+        if (record.f_t === 1) {
+          filetype = (<Icon type="folder-add" style={{ fontSize: 24, color: '#86cc32', marginRight: 16 }} />)
         }
-        return { children: <div style={{display:'flex'}}><div style={{display:'flex',flexFlow: "row",width:'500px'}}>{filetype}{text}</div>{tools}</div>
-        ,props:{style:{padding:0,margin:0,lineHeight:'30px'}}
+        return { children: <div style={{ display: 'flex' }}><div style={{ display: 'flex', flexFlow: 'row', width: '500px' }}>{filetype}
+          <EditCell editable={record.editable} value={record} updateRecord={this.updateRecord} />
+        </div>{tools}</div>,
+        props: { style: { padding: 0, margin: 0, lineHeight: '30px' } },
         }
       },
     }, {
@@ -79,28 +119,58 @@ export default class AllfileDataGrid extends Component {
    * 事件区结束
    */
 
+  editCell = (record) =>{
+    record.editable = true
+    this.state.editable = true
+    this.setState({ selectedRowKeys: [] })
+  }
+
+  updateRecord = (state,record) => {
+    alert(record.name)
+    this.state.editable = false
+  }
 
   rowClassName =(record, index) => {
-    if (this.state.selectedRowKeys.findIndex(function(value, index, arr) {
-        return value ==record.files_id
-      })!==-1) {
+    if (this.state.selectedRowKeys.findIndex((value, index, arr) => {
+      return value == record.files_id
+    }) !== -1) {
       return 'selected'
     }
     return ''
   }
 
   onRowClick = (record, index, event) => {
+    if (this.state.editable) {
+      //this.setState({ selectedRowKeys: [] })
+      return
+    }
     // record.fileselect = true;
     // this.rowSelection.selectedRowKeys=[record.customer_id]
+    if (record.f_t === 1) {
+      if (this.props.selectedFiles) {
+        this.props.selectedFiles([])
+        this.setState({ selectedRowKeys: [] })
+      }
+      return
+    }
+    if (this.props.selectedFiles) {
+      this.props.selectedFiles([record.key])
+    }
+
     this.setState({ selectedRowKeys: [record.key] })
   }
   rowKey =(record) => {
-    record.key=record.files_id
+    record.key = record.files_id
     return record.files_id
   }
 
   onRowMouseEnter =(record, index, event) => {
-    this.setState({overkey:record.files_id});
+    if(this.state.selectedRowKeys.length<2){
+      this.setState({ overkey: record.files_id })
+    }else{
+      this.setState({ overkey: 0 })
+    }
+
   }
 
   render () {
@@ -109,15 +179,25 @@ export default class AllfileDataGrid extends Component {
       selectedRowKeys,
       hideDefaultSelections: true,
       onChange: (selectedRowKeys, selectedRows) => {
-        this.setState({ selectedRowKeys });
+        if (this.props.selectedFiles) {
+          this.props.selectedFiles(selectedRows)
+        }
+
+
+        if(selectedRowKeys.length>1){
+          this.setState({ selectedRowKeys, overkey: 0  })
+        }else
+        {
+          this.setState({ selectedRowKeys})
+        }
       },
-      onSelection: this.onSelection,
-      getCheckboxProps:record => ({
-        disabled: record.f_t === 1, // Column configuration not to be checked
+      onSelection: (!this.state.editable ? this.onSelection : () => {}),
+      getCheckboxProps: record => ({
+        disabled: record.f_t === 1 || this.state.editable, // Column configuration not to be checked
       }),
     }
     return (
-        <AntDataGrid config={this.config.atgConfig} onRowMouseEnter={this.onRowMouseEnter} rowKey={this.rowKey} rowClassName={this.rowClassName} onRowClick={this.onRowClick} rowSelection={rowSelection} isPagination={false} columns={this.config.columns} ref="atg" />
+      <AntDataGrid config={this.config.atgConfig} onRowMouseEnter={this.onRowMouseEnter} rowKey={this.rowKey} rowClassName={this.rowClassName} onRowClick={this.onRowClick} rowSelection={rowSelection} isPagination={false} columns={this.config.columns} ref="atg" />
     )
   }
 }
--- src/routes/netdriver/allfile/index.js
+++ src/routes/netdriver/allfile/index.js
@@ -1,15 +1,17 @@
 import { AdminFindLayout, Component } from 'wolf'
 import AllfileDataGrid from './AllfileDataGrid'
-import { Button } from 'antd'
+import { Button ,Radio} from 'antd'
 
 export default class Allfile extends Component {
-
   /**
    *
    * 配置区
    * =======================================================================================================================
    */
-
+   state={
+     selectinfo: '',
+     filesnum:0,
+   }
 
 
   /**
@@ -19,18 +21,36 @@ export default class Allfile extends Component {
    * 事件区结束
    */
 
-
+  selectedFiles = (files) => {
+    if(files.length>0){
+      this.setState({ selectinfo: `已选中：${files.length} 个文件/文件夹`,filesnum:files.length })
+    }else{
+      this.setState({ selectinfo: '',filesnum:files.length })
+    }
+  }
 
   render () {
-
     return (
-      <AdminFindLayout topHeight={60} vspace={0}>
-        <div style={{ lineHeight: '60px', marginLeft: 10 }}>
-          <Button type="primary" icon="plus" size="large" onClick={this.addHandle} >上传</Button>
-          <Button style={{ marginLeft: 16 }} icon="plus" size="large" onClick={this.addSubHandle} >新建文件夹</Button>
-          <Button style={{ marginLeft: 16 }} icon="plus" size="large" onClick={this.delHandle} >离线下载</Button>
+      <AdminFindLayout topHeight={80} vspace={0}>
+        <div style={{ marginLeft: 10 }}>
+          <div style={{ lineHeight: '40px' }}>
+            <Button type="primary" icon="plus" size="large" onClick={this.addHandle} >上传</Button>
+            <Button style={{ marginLeft: 10 }} icon="plus" size="large" onClick={this.addSubHandle} >新建文件夹</Button>
+            <Button style={{ marginLeft: 10 }} icon="plus" size="large" onClick={this.delHandle} >离线下载</Button>
+            {
+              this.state.filesnum>0?
+              (<Button.Group size="large" style={{ marginLeft: 10 }}>
+                <Button value="large">分享</Button>
+                <Button value="default">共享</Button>
+                <Button value="small">下载</Button>
+              </Button.Group>
+              ):(<span></span>)
+            }
+
+          </div>
+          <div style={{ lineHeight: '40px' }}>{this.state.selectinfo}</div>
         </div>
-        <AllfileDataGrid ref="atg" />
+        <AllfileDataGrid ref="atg" selectedFiles={this.selectedFiles} />
       </AdminFindLayout>
     )
   }
